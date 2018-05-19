aadcd35e03c6014bfddd476b943ef9dbc48a31cd
31151021451 <38746437+31151021451@users.noreply.github.com>
2018-05-20 03:02:49 +0700

    First website upload

---
+++ JS/js1a.js
@@ -0,0 +1,10 @@
+﻿function rePlay() {
+    for (var i = 1; i <= 300; i++) {
+        setTimeout(TaoSoNgauNhien, 50 * i);
+    }
+}
+function TaoSoNgauNhien() {
+    max = parseInt(document.getElementsByName("giatri")[0].value);
+    a = parseInt(Math.random() * (max) + 1);
+    document.getElementsByName("kq")[0].value = a;
+}
---
+++ JS/js1b.js
@@ -0,0 +1,34 @@
+﻿function rePlay1() {
+    for (var i = 1; i <= 300; i++) {
+        setTimeout(TaoDaySoNgauNhien, 50 * i);
+    }
+}
+function TaoDaySoNgauNhien() {
+
+    document.getElementsByName("ketqua")[0].value = "";
+    max = parseInt(document.getElementsByName("giatri")[0].value);
+    var b = new Array(max);
+    var c = '';
+    var m = '';
+    var n = '';
+    var temp = '';
+    for (var i = 0; i < b.length; i++) {
+        b[i] = i + 1;
+    }
+    for (var j = 0; j < b.length; j++) {
+        m = parseInt(Math.random() * (max));
+        n = parseInt(Math.random() * (max));
+        temp = b[m];
+        b[m] = b[n];
+        b[n] = temp;
+    }
+    for (var k = 0; k < b.length; k++) {
+        if (k == b.length - 1) {
+            c += b[k];
+        }
+        else c += b[k] + ',';
+    }
+
+
+    document.getElementsByName("ketqua")[0].value = c;
+}
